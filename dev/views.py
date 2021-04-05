d6eb8eeea81419b9c5e32ef4467bf25db1cdd91a
Baratinus <p.baratinus@gmail.com>
2021-04-05 22:01:16 +0200

    test favicon

--- dev/views.py
+++ dev/views.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request,send_from_directory
 
 app = Flask(__name__)
 
@@ -8,5 +8,10 @@ app.config.from_object('config')
 @app.route('/index/')
 def index():
     return render_template("index.html")
+
+@app.route('/favicon.ico')
+def favicon():
+    print(app.config["PATH"])
+    return send_from_directory(os.path.join(app.config["PATH"], 'static'), '/favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 app.run(debug=True, port=app.config["PORT"], host=app.config["IP"])
