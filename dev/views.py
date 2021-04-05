4967680f33d8965506110790a9db78e020d83437
Baratinus <p.baratinus@gmail.com>
2021-04-05 22:16:03 +0200

    favicon final (peut être)

--- dev/views.py
+++ dev/views.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,request,send_from_directory
+from flask import Flask,render_template,request
 
 app = Flask(__name__)
 
@@ -8,10 +8,5 @@ app.config.from_object('config')
 @app.route('/index/')
 def index():
     return render_template("index.html")
-
-@app.route('/favicon.ico')
-def favicon():
-    print(app.config["PATH"])
-    return send_from_directory(os.path.join(app.config["PATH"], 'static/img'), 'favicon.ico')
 
 app.run(debug=True, port=app.config["PORT"], host=app.config["IP"])
