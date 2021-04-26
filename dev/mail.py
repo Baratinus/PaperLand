781c0e15642d8cd3bde064ce274dd36fe5e704ec
Miroredge <Maximilien.poncet19@gmail.com>
2021-04-26 19:22:54 +0200

    Recovery 2

--- dev/mail.py
+++ dev/mail.py
@@ -2,23 +2,36 @@ import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
-<<<<<<< HEAD
-def sendmail (receiver_address, mail_content_password, sender_address="paper.noreply@gmail.com", sender_pass="papernoreplypassword") :
-=======
 def sendmail (receiver_address, mail_content_password, mail_content_choose='mail_content_base', sender_address="paper.noreply@gmail.com", sender_pass="papernoreplypassword") :
->>>>>>> parent of f6efd6b (Merge pull request #7 from Baratinus/admin)
     """
    -RecieverAdress
    -MailContent(UserPassword)
     """
     
-    mail_content ="Bonjour \nVotre uuid temporaire est : " + mail_content_password + "\nCette clée est à utiliser au même titre que votre mot de passe."
+    mail_content ="Bonjour \nVotre uuid temporaire est : " + mail_content_password + "\nCette clé est à utiliser au même titre que votre mot de passe."
     
     message = MIMEMultipart()
     message['From'] = sender_address
     message['To'] = receiver_address
-    message['Subject'] = "Forgot-Password-PaperLand" 
-    message.attach(MIMEText(mail_content, 'plain'))
+
+    if mail_content_choose == 'mail_content_base' :
+        message['Subject'] = "Forgot-Password-PaperLand"
+        message.attach(MIMEText(mail_content, 'plain'))
+
+    elif mail_content_choose == 'notify_update_password' :
+        message['Subject'] = "Update-Password-PaperLand"
+        mail_content_update = "Bonjour \nNous vous informons que votre mot de passe à bien été modifié !\nA bientôt !"
+        message.attach(MIMEText(mail_content_update, 'plain'))
+
+    elif mail_content_choose == 'notify_account_created' :
+        message['Subject'] = "Account-Created-On-Paperland"
+        mail_content_update = "Bonjour \nNous vous informons que votre compte a bien été enregistré !!!\nA bientôt !"
+        message.attach(MIMEText(mail_content_update, 'plain'))
+    
+    elif mail_content_choose == 'notify_account_deleted' :
+        message['Subject'] = "Account-Deleted-On-Paperland"
+        mail_content_update = "Bonjour \nNous vous informons que votre compte a bien été supprimé :( \nA bientôt, Bonne continuation !"
+        message.attach(MIMEText(mail_content_update, 'plain'))
 
     session = smtplib.SMTP('smtp.gmail.com', 587) 
     session.starttls()
